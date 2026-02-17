730d2b77fa6f0c0898af7f106a57baef1246e5b8
habib <habibguesmi@gmail.com>
2026-02-17 11:22:37 +0100

    Remove node_modules and dist from repository

--- dist/difference-score.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function getDifferenceScore(a: string, b: string): number;
-//# sourceMappingURL=difference-score.d.ts.map
--- dist/suggestion-engine.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class SuggestionEngine {
-    private readonly words;
-    private computeScore;
-    constructor(words: string[]);
-    getSuggestions(term: string, maxResults: number): string[];
-}
-//# sourceMappingURL=suggestion-engine.d.ts.map
